4d67ba2e17dc2854b866c66282ba2c9a49e7667a
lipengcheng3 <lipengcheng3@jd.com>
2020-04-14 10:55:06 +0800

    add: 把之前 zk 官网的 demo 弄下来了

---
+++ init/src/main/java/com/lpc/learn/zk/base/example/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Package: com.lpc.learn.zk.base.example
+ * User: 李鹏程
+ * Email: lipengcheng3@jd.com
+ * Date: 2020/4/9
+ * Time: 20:51
+ * Description: 这个包下面的两个类是我从 zk 官网的 java demo 拷过来的。官网的直接在 html 看代码有点费劲
+ */
+package com.lpc.learn.zk.base.example;
