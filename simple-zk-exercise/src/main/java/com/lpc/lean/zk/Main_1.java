f2ad90948de3edd1dd386ae7b537634ac7b727b4
lipengcheng3 <lipengcheng3@jd.com>
2020-04-30 18:25:41 +0800

    save: 从最基本的开始

---
+++ simple-zk-exercise/src/main/java/com/lpc/lean/zk/Main_1.java
@@ -0,0 +1,16 @@
+package com.lpc.lean.zk;
+
+/**
+ * Package: com.lpc.lean.zk
+ * User: 李鹏程
+ * Email: lipengcheng3@jd.com
+ * Date: 2020/4/30
+ * Time: 18:06
+ * Description: 进行zk的最基本功能的实践，不要贪多了。
+ * 此实例
+ */
+
+public class Main_1 {
+    public static void main(String[] args) {
+    }
+}
