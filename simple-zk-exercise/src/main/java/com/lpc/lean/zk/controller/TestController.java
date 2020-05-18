a146d746b26f115bbd5eed8d24137446559f622a
lipengcheng3 <lipengcheng3@jd.com>
2020-05-18 17:40:24 +0800

    modify: 修改 springboot 启动后立刻跑完的问题——增加tomcat

---
+++ simple-zk-exercise/src/main/java/com/lpc/lean/zk/controller/TestController.java
@@ -0,0 +1,27 @@
+package com.lpc.lean.zk.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Package: com.lpc.lean.zk.controller
+ * User: 李鹏程
+ * Email: lipengcheng3@jd.com
+ * Date: 2020/5/18
+ * Time: 17:34
+ * Description:
+ */
+@Slf4j
+@RestController
+@RequestMapping(path = "/test")
+public class TestController {
+
+    @GetMapping(path = "")
+    public String test(){
+        log.info("收到一次请求");
+        return "haha";
+    }
+}
